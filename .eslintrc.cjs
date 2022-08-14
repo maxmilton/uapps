dbd32e548aa6e81d43341e5a71e1e4333c4356e6
Max Milton <max@happycode.ltd>
2022-08-14 17:54:22 +1000

    chore: Improve lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,7 @@
 const OFF = 0;
 const WARN = 1;
+const ERROR = 2;
 
-// TODO: Types
-// eslint-disable-next-line max-len
-// /** @type {import('eslint/lib/shared/types').ConfigData & { parserOptions: import('@typescript-eslint/types').ParserOptions }} */
 module.exports = {
   root: true,
   reportUnusedDisableDirectives: true,
@@ -23,15 +21,15 @@ module.exports = {
     'plugin:security/recommended',
   ],
   rules: {
-    '@typescript-eslint/explicit-module-boundary-types': WARN,
+    '@typescript-eslint/explicit-module-boundary-types': ERROR,
     'import/prefer-default-export': OFF,
+    'no-restricted-syntax': OFF,
     'unicorn/filename-case': OFF,
     'unicorn/no-abusive-eslint-disable': WARN,
     'unicorn/no-null': OFF,
     'unicorn/prefer-add-event-listener': OFF,
     'unicorn/prefer-dom-node-append': OFF,
-    'unicorn/prefer-module': OFF,
-    'unicorn/prefer-node-protocol': OFF,
+    'unicorn/prefer-module': WARN,
     'unicorn/prefer-query-selector': OFF,
     'unicorn/prevent-abbreviations': OFF,
   },
