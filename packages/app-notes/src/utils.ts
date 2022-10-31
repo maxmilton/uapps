b98d7e0e6272bf1597245398401b886e3a4e9453
Max Milton <max@happyhackers.xyz>
2022-10-31 14:39:01 +1100

    Move store into stage1

--- packages/app-notes/src/utils.ts
+++ packages/app-notes/src/utils.ts
@@ -1,59 +1,9 @@
 import type { Session } from '@supabase/supabase-js';
 import { prepend } from 'stage1';
+import { store } from 'stage1/store';
 import { Nav } from './components/Nav';
 import { routeTo } from './router';
 import { supabase } from './supabase';
-
-type Handler<T, K extends keyof T> = (value: T[K], prev: T[K]) => any;
-type Handlers<T> = Record<keyof T, Handler<T, keyof T>[]>;
-type StoreOn<T> = <K extends keyof T>(key: K, callback: Handler<T, K>) => void;
-type StoreOff<T> = <K extends keyof T>(key: K, callback: Handler<T, K>) => void;
-
-export function store<T extends Record<string, any>>(
-  initialState: T,
-): T & { on: StoreOn<T>; off: StoreOff<T> } {
-  const handlers = {} as Handlers<T>;
-
-  const proxy = new Proxy(initialState, {
-    set(target, property, value, receiver) {
-      if (typeof property === 'string' && handlers[property]) {
-        for (const listener of handlers[property]) {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-          listener(value, target[property]);
-        }
-      }
-      return Reflect.set(target, property, value, receiver);
-    },
-  }) as T & { on: StoreOn<T>; off: StoreOff<T> };
-
-  proxy.on = (property, callback) => {
-    // @ts-expect-error- FIXME:!
-    (handlers[property] ??= []).push(callback);
-  };
-  proxy.off = (property, callback) => {
-    // @ts-expect-error- FIXME:!
-    // eslint-disable-next-line no-bitwise
-    handlers[property]?.splice(handlers[property].indexOf(callback) >>> 0, 1);
-  };
-
-  // Object.defineProperties(proxy, {
-  //   on: {
-  //     value: (property, callback) => {
-  //       (handlers[property] ??= []).push(callback);
-  //     },
-  //   },
-  //   off: {
-  //     value: (property, callback) => {
-  //       handlers[property]?.splice(
-  //         handlers[property].indexOf(callback) >>> 0,
-  //         1,
-  //       );
-  //     },
-  //   },
-  // });
-
-  return proxy;
-}
 
 export const state = store({
   session: null as Session | null,
