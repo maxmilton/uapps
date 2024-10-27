cf675f7436219c2501435eedfbbc38ca8eba754a
Max Milton <max@happyhackers.xyz>
2024-10-27 21:50:57 +0900

    chore: Refactor ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,8 @@
-import { fixupPluginRules } from '@eslint/compat';
-import { FlatCompat } from '@eslint/eslintrc';
 import eslint from '@eslint/js';
+import mm from '@maxmilton/eslint-config';
 import security from 'eslint-plugin-security';
 import unicorn from 'eslint-plugin-unicorn';
-// eslint-disable-next-line import/no-unresolved
 import tseslint from 'typescript-eslint';
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
 
 const OFF = 0;
 const WARN = 1;
@@ -16,69 +10,33 @@ const ERROR = 2;
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...compat.extends('airbnb-base').map((config) => ({
-    ...config,
-    plugins: {}, // delete
-  })),
-  ...compat.extends('airbnb-typescript/base'),
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   unicorn.configs['flat/recommended'],
+  mm.configs.recommended,
   security.configs.recommended,
   {
     linterOptions: {
-      reportUnusedDisableDirectives: WARN,
+      reportUnusedDisableDirectives: ERROR,
     },
     languageOptions: {
       parserOptions: {
         project: ['tsconfig.json', 'tsconfig.node.json'],
+        projectService: {
+          allowDefaultProject: ['*.js', '*.cjs', '*.mjs'],
+        },
         tsconfigRootDir: import.meta.dirname,
       },
     },
-    // settings: {
-    //   'import/resolver': {
-    //     typescript: {
-    //       alwaysTryTypes: true,
-    //       project: ['tsconfig.json', 'tsconfig.node.json'],
-    //       // project: true,
-    //     },
-    //   },
-    // },
-    plugins: {
-      import: fixupPluginRules(
-        compat.plugins('eslint-plugin-import')[0].plugins?.import ?? {},
-      ),
-    },
     rules: {
-      '@typescript-eslint/explicit-module-boundary-types': ERROR,
-      '@typescript-eslint/no-confusing-void-expression': WARN,
-      '@typescript-eslint/restrict-plus-operands': WARN,
-      'import/prefer-default-export': OFF,
-      'no-restricted-syntax': OFF,
-      'no-void': OFF,
-      'unicorn/filename-case': OFF,
-      'unicorn/import-style': WARN,
-      'unicorn/no-abusive-eslint-disable': WARN,
-      'unicorn/no-null': OFF,
-      'unicorn/prefer-module': WARN,
-      'unicorn/prefer-top-level-await': WARN,
-      'unicorn/prevent-abbreviations': OFF,
       // bad browser support
       'unicorn/prefer-at': OFF,
       // prefer to clearly separate Bun and DOM
       'unicorn/prefer-global-this': OFF,
 
-      /* Covered by biome formatter */
-      '@typescript-eslint/indent': OFF,
-      'function-paren-newline': OFF,
-      'implicit-arrow-linebreak': OFF,
-      'max-len': OFF,
-      'object-curly-newline': OFF,
-      'operator-linebreak': OFF,
-      'unicorn/no-nested-ternary': OFF,
-
       /* Performance and byte savings */
       // byte savings
+      '@typescript-eslint/restrict-plus-operands': WARN,
       'no-plusplus': OFF,
       'unicorn/no-array-callback-reference': WARN,
       // forEach is often faster (in Chrome and Firefox but not Safari)
@@ -97,22 +55,13 @@ export default tseslint.config(
     },
   },
   {
-    files: [
-      '*.config.mjs',
-      '*.config.ts',
-      '*.d.ts',
-      '*.spec.ts',
-      '*.test.ts',
-      'packages/*/*.config.ts',
-      'packages/*/build.ts',
-      'packages/*/test/**',
-      'test/**',
-    ],
+    files: ['packages/*/build.ts'],
     rules: {
-      'import/no-extraneous-dependencies': OFF,
+      'no-await-in-loop': OFF,
+      'no-console': OFF,
     },
   },
   {
-    ignores: ['*.bak', 'packages/*/dist/**'],
+    ignores: ['*.bak', 'coverage/**', 'packages/*/dist/**'],
   },
 );
