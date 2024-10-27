f5eaefcd862a2e86deaca7d48e59db7b796f676f
Max Milton <max@happyhackers.xyz>
2024-10-27 21:51:52 +0900

    test: Refactor to use `@maxmilton/test-utils` package

--- test/setup.ts
+++ test/setup.ts
@@ -1,56 +1,8 @@
-import { expect } from 'bun:test';
-import { GlobalWindow, type Window } from 'happy-dom';
+import '@maxmilton/test-utils/extend';
 
-declare global {
-  /** Real bun console. `console` is mapped to happy-dom's virtual console. */
-  // eslint-disable-next-line no-var, vars-on-top
-  var console2: Console;
-  // eslint-disable-next-line no-var, vars-on-top
-  var happyDOM: Window['happyDOM'];
-}
+import { setupDOM } from '@maxmilton/test-utils/dom';
 
-declare module 'bun:test' {
-  interface Matchers {
-    /** Asserts that a value is a plain `object`. */
-    toBePlainObject(): void;
-  }
-}
-
-expect.extend({
-  // XXX: Although bun has a `toBeObject` matcher, it's not as useful since it
-  // doesn't check for plain objects.
-  toBePlainObject(received: unknown) {
-    return Object.prototype.toString.call(received) === '[object Object]'
-      ? { pass: true }
-      : {
-          pass: false,
-          message: () => `expected ${String(received)} to be a plain object`,
-        };
-  },
-});
-
-const originalConsole = global.console;
 const noop = () => {};
-
-function setupDOM() {
-  const dom = new GlobalWindow({
-    // TODO: Decide if we want to use the virtual console or not
-    //  ↳ https://github.com/capricorn86/happy-dom/wiki/Virtual-Console
-    // console: global.console,
-  });
-  global.happyDOM = dom.happyDOM;
-  global.console2 = originalConsole;
-  // @ts-expect-error - happy-dom only implements a subset of the DOM API
-  global.window = dom.window.document.defaultView;
-  global.document = window.document;
-  global.console = window.console;
-  global.fetch = window.fetch;
-  global.setTimeout = window.setTimeout;
-  global.clearTimeout = window.clearTimeout;
-  global.DocumentFragment = window.DocumentFragment;
-  global.CSSStyleSheet = window.CSSStyleSheet;
-  global.Text = window.Text;
-}
 
 function setupMocks(): void {
   // @ts-expect-error - noop stub
