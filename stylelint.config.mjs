495dbacef582e8c76f2b8a60f579328b3bf6680f
Max Milton <max@happyhackers.xyz>
2025-02-24 11:34:21 +0900

    chore: Ignore Stylelint rule

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -5,6 +5,7 @@ export default {
   extends: ['stylelint-config-standard', '@maxmilton/stylelint-config'],
   ignoreFiles: ['**/*.bak/**', '**/dist/**', '**/node_modules/**'],
   rules: {
+    'declaration-property-value-no-unknown': null, // TODO: Enable this rule + implement in postcss-ekscss
     'import-notation': null,
     'function-name-case': null,
     'function-no-unknown': null,
