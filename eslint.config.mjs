18b9eb662156c6a2e04e118995a322a2d6752f01
Max Milton <max@2okki.com>
2025-08-25 09:58:54 +0900

    chore: Clean up eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,6 @@ import oxlint from "eslint-plugin-oxlint";
 import unicorn from "eslint-plugin-unicorn";
 import ts from "typescript-eslint";
 
-const OFF = 0;
-const ERROR = 2;
-
 /** @type {import("typescript-eslint").ConfigArray} */
 const config = ts.config(
   js.configs.recommended,
@@ -17,7 +14,7 @@ const config = ts.config(
   oxlint.configs["flat/all"],
   {
     linterOptions: {
-      reportUnusedDisableDirectives: ERROR,
+      reportUnusedDisableDirectives: "error",
     },
     languageOptions: {
       parserOptions: {
@@ -33,27 +30,7 @@ const config = ts.config(
       },
     },
     rules: {
-      quotes: [ERROR, "double", { avoidEscape: true }],
-
-      // Bad browser support
-      "unicorn/prefer-at": OFF,
-      // Prefer to clearly separate Bun and DOM
-      "unicorn/prefer-global-this": OFF,
-
-      /* Performance and byte savings */
-      // byte savings
-      "no-plusplus": OFF,
-      // forEach is often faster
-      "unicorn/no-array-for-each": OFF,
-      // byte savings (minification doesn't currently automatically remove)
-      "unicorn/switch-case-braces": [ERROR, "avoid"],
-
-      /* stage1 */
-      // Underscores in synthetic event handler names
-      "no-underscore-dangle": OFF,
-      "unicorn/prefer-add-event-listener": OFF,
-      "unicorn/prefer-dom-node-append": OFF,
-      "unicorn/prefer-query-selector": OFF,
+      quotes: ["error", "double", { avoidEscape: true }],
     },
   },
   {
